cea4aeddfc574b74eee7765279cce802bde6a156
SalaheddineBensaghir <bensaghir2000@gmail.com>
2024-06-01 10:57:28 +0100

    version 9

--- src/main/java/com/sid/ebankingbackend/web/BankAccountRestAPI.java
+++ src/main/java/com/sid/ebankingbackend/web/BankAccountRestAPI.java
@@ -7,15 +7,12 @@ import com.sid.ebankingbackend.exception.BankAccountNotFoundException;
 import com.sid.ebankingbackend.services.BankAccountService;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-
+@CrossOrigin("*")
 public class BankAccountRestAPI {
 
     private BankAccountService bankAccountService;
